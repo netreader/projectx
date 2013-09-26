c46e153f063ec8ecc7e036ebfa5663efa29ad4d7
Net Reader <n3tr3ad3r@gmail.com>
2013-09-27 00:03:47 +0530

    added lib,conf,properties etc and created a build file

---
+++ src/com/px/base/BaseConnection.java
@@ -0,0 +1,14 @@
+package com.px.base;
+
+import java.sql.Connection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BaseConnection {
+
+	private static Logger log = LoggerFactory.getLogger(BaseConnection.class);
+	
+	private Connection connection = null;
+	
+}
